c4629eee3c954fc3430189386f9337dde8c23112
DixonJ2 <dixon.joshua2@student.oaklandcc.edu>
2026-03-09 20:37:09 -0400

    Fix: update RELEASE_API_DOMAIN to static IP 34.68.160.91

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,8 @@
 // Local emulator backend; release builds should hit your LAN IP so physical devices can reach the host.
 const LOCAL_API_DOMAIN = 'http://10.0.2.2:3001';
 // Cloud VM backend (Google Compute Engine solar-api, us-central1-c)
-const RELEASE_API_DOMAIN = 'http://34.133.28.185:3001';
+// Static IP - reserved in GCP as 'solar-api-static-ip', will not change on VM restart
+const RELEASE_API_DOMAIN = 'http://34.68.160.91:3001';
 
 export const API_DOMAIN = __DEV__ ? LOCAL_API_DOMAIN : RELEASE_API_DOMAIN;
 export const API_BASE_URL = `${API_DOMAIN}/api`;
